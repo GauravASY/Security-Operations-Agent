86d0dabb3110841472a59f68d65bd484c121afa8
Gaurav <gauravyadav199808@gmail.com>
2025-12-12 16:43:00 +0530

    Model changed to Local Ollama Model

--- agent.py
+++ agent.py
@@ -2,15 +2,18 @@ from agents import Agent
 from agents.extensions.models.litellm_model import LitellmModel
 import os
 from prompt import career_assistant_prompt
+from tools import get_list_of_jobs
 
 
 google_model = LitellmModel(
-    model="gemini/gemini-2.5-flash",  # specific LiteLLM model identifier
-    api_key=os.environ.get("GEMINI_API_KEY")
+    model="ollama/gemma3:latest",  # specific LiteLLM model identifier
+    base_url="http://localhost:11434",
+    api_key="ollama"
 )
 
 career_assistant = Agent(
     name= "Gaurav",
     instructions= career_assistant_prompt,
-    model = google_model
+    model = google_model,
+    tools = [get_list_of_jobs]
 )
