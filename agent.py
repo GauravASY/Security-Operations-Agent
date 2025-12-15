01a721f86e71194877d7fcba63da1a7cccaa92eb
Gaurav <gauravyadav199808@gmail.com>
2025-12-15 19:06:30 +0530

    Changed Model to redhat Litemaas Mistral

--- agent.py
+++ agent.py
@@ -1,19 +1,32 @@
-from agents import Agent
+from agents import Agent, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
+from openai import AsyncOpenAI
+from agents.models.openai_chatcompletions import OpenAIChatCompletionsModel
 import os
 from prompt import career_assistant_prompt
 from tools import get_list_of_jobs
 
+set_tracing_disabled(True)
 
+#keeping this model for testing purposes.
 google_model = LitellmModel(
     model="ollama/llama3:latest",  # specific LiteLLM model identifier
     base_url="http://localhost:11434",
     api_key="ollama"
 )
+custom_client = AsyncOpenAI(
+    base_url=os.environ.get("LMAAS_URL"),
+    api_key=os.environ.get("LMAAS_KEY"),
+)
+
+custom_model = OpenAIChatCompletionsModel(
+    openai_client=custom_client,
+    model=os.environ.get("LMAAS_MODEL"),
+)
 
 career_assistant = Agent(
     name= "Gaurav",
     instructions= career_assistant_prompt,
-    model = google_model,
+    model = custom_model,
     tools = [get_list_of_jobs]
 )
