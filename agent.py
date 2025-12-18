ea28d7908692a16df129d4c9c3b6891c9bbb5203
Gaurav <gauravyadav199808@gmail.com>
2025-12-18 19:55:51 +0530

    Implemented Analysis Delegation to Analysis Agent

--- agent.py
+++ agent.py
@@ -1,12 +1,15 @@
-from agents import Agent, set_tracing_disabled
+from agents import Agent, set_tracing_disabled, handoff
 from agents.extensions.models.litellm_model import LitellmModel
 from openai import AsyncOpenAI
 from agents.models.openai_chatcompletions import OpenAIChatCompletionsModel
 import os
-from prompt import career_assistant_prompt
+from prompt import career_assistant_prompt, analysis_agent_prompt
 from tools import get_list_of_jobs, search_knowledge_base
 
 set_tracing_disabled(True)
+
+def log_analyses_handoff(context):
+    yield "```Delegating Analyses to Analysis Agent```\n\n"
 
 #keeping this model for testing purposes.
 google_model = LitellmModel(
@@ -24,9 +27,24 @@ custom_model = OpenAIChatCompletionsModel(
     model=os.environ.get("LMAAS_MODEL"),
 )
 
+analysis_agent = Agent(
+    name= "Analysis Agent",
+    instructions= analysis_agent_prompt,
+    handoff_description="Handles analyses of the pdf for threats and provides recommendations",
+    model = custom_model,
+    tools = [search_knowledge_base]
+)
+
 career_assistant = Agent(
     name= "Gaurav",
     instructions= career_assistant_prompt,
+    handoffs=[
+        handoff(
+            agent=analysis_agent,
+            tool_name_override="analyze_pdf",
+            on_handoff = log_analyses_handoff
+        )
+    ],
     model = custom_model,
     tools = [get_list_of_jobs, search_knowledge_base]
 )
