0533e5e4f4164e550e5947512c2b6c82f1e628ed
Gaurav <gauravyadav199808@gmail.com>
2025-12-30 23:15:24 +0530

    Provided content tool to main agent

--- llmAgent.py
+++ llmAgent.py
@@ -4,7 +4,7 @@ from openai import AsyncOpenAI
 from agents.models.openai_chatcompletions import OpenAIChatCompletionsModel
 import os
 from prompt import career_assistant_prompt, extraction_agent_prompt
-from tools import search_knowledge_base
+from tools import search_knowledge_base, search_indicators_by_report, search_by_victim, get_file_content
 from typing import List, Optional
 from pydantic import BaseModel, Field
 
@@ -61,5 +61,5 @@ career_assistant = Agent(
         )
     ],
     model = custom_model,
-    tools = [search_knowledge_base]
+    tools = [search_indicators_by_report, search_by_victim, get_file_content]
 )
