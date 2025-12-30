28092830c8b2372f83e88755b0c5814f634d51b1
Gaurav <gauravyadav199808@gmail.com>
2025-12-30 23:14:43 +0530

    Added duplicacy check for uploaded file

--- vectorstore.py
+++ vectorstore.py
@@ -27,11 +27,17 @@ async def ingest_txt(file_path, s3_url):
         print("Text : \n", text)
         extracted_data = await Runner.run(extraction_assistant, content)
         data = extracted_data.final_output
-
+        
+        file_path = file_path.split("\\")[-1]
         
         conn = psycopg2.connect(dbname=TARGET_DB, **DB_CONFIG)
         cur = conn.cursor()
         if len(text) > 0:
+            #check whether the file is already ingested
+            cur.execute("SELECT * FROM reports WHERE filename = %s", (file_path,))
+            if cur.fetchone():
+                return {"success" : True, "message" : "File already ingested"}
+            
             cur.execute("""
                 INSERT INTO reports (filename, summary, severity, victim_sector, timeline_start, timeline_end, raw_content)
                 VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING report_id;
@@ -59,12 +65,12 @@ async def ingest_txt(file_path, s3_url):
                 ids=[str(uuid.uuid4()) for _ in range(len(text))]
             )
             print(f"--> Successfully ingested Report ID: {report_id}")
-            return {"success" : True}
+            return {"success" : True, "message" : "File processed successfully"}
         
     except Exception as e:
         print(f"Error ingesting {file_path}: {e}")
         conn.rollback()
-        return {"success" : False}
+        return {"success" : False, "message" : "File processing failed"}
     finally:
         conn.close()
     
